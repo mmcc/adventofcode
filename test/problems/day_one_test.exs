89439c6d32abee3dbe9c8ceec0565577b2650cb7
Matthew McClure <m@mmcc.io>
2015-12-18 09:06:55 -0800

    use assertions in day one tests

--- test/problems/day_one_test.exs
+++ test/problems/day_one_test.exs
@@ -3,19 +3,19 @@ defmodule AdventOfCodeTest.DayOne do
   doctest AdventOfCode
 
   test "what_floor returns the final floor and the index of the floor that sends Santa to the basement" do
-    {:ok, _, 0} = AdventOfCode.DayOne.what_floor("(())")
-    {:ok, _, 0} = AdventOfCode.DayOne.what_floor("()()")
-    {:ok, _, 3} = AdventOfCode.DayOne.what_floor("(((")
-    {:ok, _, 3} = AdventOfCode.DayOne.what_floor("(()(()(")
-    {:ok, _, 3} = AdventOfCode.DayOne.what_floor("))(((((")
-    {:ok, _, -1} = AdventOfCode.DayOne.what_floor("())")
-    {:ok, _, -1} = AdventOfCode.DayOne.what_floor("))(")
-    {:ok, _, -3} = AdventOfCode.DayOne.what_floor(")))")
-    {:ok, _, -3} = AdventOfCode.DayOne.what_floor(")())())")
+    assert {:ok, _, 0} = AdventOfCode.DayOne.what_floor("(())")
+    assert {:ok, _, 0} = AdventOfCode.DayOne.what_floor("()()")
+    assert {:ok, _, 3} = AdventOfCode.DayOne.what_floor("(((")
+    assert {:ok, _, 3} = AdventOfCode.DayOne.what_floor("(()(()(")
+    assert {:ok, _, 3} = AdventOfCode.DayOne.what_floor("))(((((")
+    assert {:ok, _, -1} = AdventOfCode.DayOne.what_floor("())")
+    assert {:ok, _, -1} = AdventOfCode.DayOne.what_floor("))(")
+    assert {:ok, _, -3} = AdventOfCode.DayOne.what_floor(")))")
+    assert {:ok, _, -3} = AdventOfCode.DayOne.what_floor(")())())")
   end
 
   test "what_floor returns the index at which Santa had to go to the basement" do
-    {:ok, 1, _} = AdventOfCode.DayOne.what_floor(")")
-    {:ok, 5, _} = AdventOfCode.DayOne.what_floor("()())")
+    assert {:ok, 1, _} = AdventOfCode.DayOne.what_floor(")")
+    assert {:ok, 5, _} = AdventOfCode.DayOne.what_floor("()())")
   end
 end
